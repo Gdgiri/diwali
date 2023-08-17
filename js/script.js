9460de16bc83a895183a66080a14a76926e3c29d
Giridharan.S <96366499+Gdgiri@users.noreply.github.com>
2023-08-17 21:21:35 +0530

    Add files via upload
    save

---
+++ js/script.js
@@ -0,0 +1,36 @@
+
+
+
+const name=window.prompt("what is your name");
+
+// const wish=window.prompt("Enter your wishes")
+
+
+
+
+const man=document.getElementById('man');
+man.innerHTML=`HAPPY DIWALI TO YOU ${name}`.toUpperCase();
+// var loadFile = function(event) {
+//     var image = document.getElementById('output');
+//     image.src=URL.createObjectURL(event.target.files[0]);
+// };
+
+const id=document.getElementById('names');
+
+
+id.innerHTML=`HAPPY DIWALI TO YOU  ${name}`;
+
+
+
+const ids=document.getElementById('get');
+
+ids.innerHTML=`HI! HAPPY DIWALI TO YOU ${name} `.toUpperCase();
+
+// document.addEventListener('DOMContentLoaded',()=>{
+//     const ccc=document.getElementById('cake');
+//     ids.innerHTML
+// })
+
+
+
+
